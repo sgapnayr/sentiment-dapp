5253bcad5aa9a582ad0aec6b31a427ee00ad8988
Pags <programwithpags@gmail.com>
2024-02-16 22:01:08 -0600

    Clean Code

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import BaseBanner from "@/components/BaseBanner";
+import BaseGrowingEcosystem from "@/components/BaseGrowingEcosystem";
 import DefaultLayout from "@/layout/default-layout";
 
 export default function Home() {
   return (
     <DefaultLayout>
       <BaseBanner />
+      <BaseGrowingEcosystem />
     </DefaultLayout>
   );
 }
---
+++ components/BaseGrowingEcosystem.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function BaseGrowingEcosystem() {
+  return <div>BaseGrowingEcosystem</div>;
+}
